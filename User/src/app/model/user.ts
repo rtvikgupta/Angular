0539177484c0a1b5910686f8b5f932626455da20
rtvikgupta <guptartvik@gmail.com>
2020-05-14 16:23:18 +0530

    commit

--- User/src/app/model/user.ts
+++ User/src/app/model/user.ts
@@ -1,2 +1,19 @@
 export class User {
+    userId:String;
+    userPassword:String;
+    userName:String;
+    contactNo:String;
+    userEmail:String;
+    age:number;
+    gender:String;
+
+    constructor(userId:String,userPassword:String,userName:String,contactNo:String,userEmail:String,age:number,gender:String){
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.userName = userName;
+        this.contactNo = contactNo;
+        this.userEmail = userEmail;
+        this.age = age;
+        this.gender =gender;
+    }
 }
--- User/src/app/register/register.component.ts
+++ User/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  user:User;
+  flag:boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  submit(userform:any) {
+      let details = userform.value;
+      let name = details.name;
+      let password = details.password;
+      let email = details.email;
+      let contact = details.contact;
+      let age = details.age;
+      let gender = details.gender;
+      this.user = new User("101",password,name,contact,email,age,gender);
+      this.flag = true;
   }
 
 }
