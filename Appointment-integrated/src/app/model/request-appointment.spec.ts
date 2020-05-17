364b21164c8a88aea0bcc4d7963d2f5c54696038
rtvikgupta <guptartvik@gmail.com>
2020-05-17 13:00:58 +0530

    commit

--- Appointment-integrated/src/app/model/request-appointment.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RequestAppointment } from './request-appointment';
-
-describe('RequestAppointment', () => {
-  it('should create an instance', () => {
-    expect(new RequestAppointment()).toBeTruthy();
-  });
-});
